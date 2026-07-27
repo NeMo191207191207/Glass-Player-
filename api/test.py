30b0062297cb51a81b01f5c142476c2e83aeb9ba
Alen Troyan <alentroyan@gmail.com>
2026-07-27 23:39:46 +0300

    Use BaseHTTPRequestHandler for test API

--- api/test.py
+++ api/test.py
@@ -1,6 +1,16 @@
-def handler(request):
-    return {
-        'status': 200,
-        'headers': {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
-        'body': '{"message": "API works!"}'
-    }
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write(json.dumps({'message': 'API works!'}).encode())
+    
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.end_headers()
